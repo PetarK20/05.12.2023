949139590480b8a4ee5bb8d6dde77f9189a94001
PetarK20 <148004863+PetarK20@users.noreply.github.com>
2023-12-04 18:00:17 +0200

    Update zd8.py

--- zd8.py
+++ zd8.py
@@ -1,7 +1,19 @@
 n = int(input())
 matrix = []
-
 for _ in range(n):
-    UserInput = list(map(int, input().split(', ')))
+    UserInput = list(input())
     matrix.append(UserInput)
-print(matrix)
+    flat_list = sum(matrix, [])
+
+target_symbol = input()
+
+reda = 0
+while reda < n:
+    if target_symbol in flat_list:
+        for i in flat_list:
+            index1 = UserInput.index(target_symbol)
+        print(f"({index1}, {matrix.index(target_symbol)})")
+        break
+    reda += 1
+else:
+    print(f"{target_symbol} does not exist in matrix!")
