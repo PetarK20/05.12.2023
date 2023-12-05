43eef959128304d795b5dcb0940992581743c2e7
PetarK20 <148004863+PetarK20@users.noreply.github.com>
2023-12-05 12:30:00 +0200

    Update zd3.py

--- zd3.py
+++ zd3.py
@@ -5,12 +5,9 @@ for i in range(num):
     for j in range(num):
         mtrix[i].append(j + 1 + i * num)
 
-sum1 = sum(mtrix[0][0:])
-sum2 = sum(mtrix[1][0:])
-sum3 = sum(mtrix[2][0:])
+for i in range(num):
+    print(sum(mtrix[i]))
 
 
 print(*mtrix, sep="\n")
-print(sum1)
-print(sum2)
-print(sum3)
+
